602f93628b007e4e86f3ff2c20159910aec0eec2
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-01-23 22:44:46 +0900

    ✨ APIを使用して画像検索アプリ作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ImageGallery from "./ImageGallery";
+import { useRef, useState } from "react";
 
 function App() {
+  const [fetchData, setFetchData] = useState([]);
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+
+    //API_URL
+    const endPonintURL = `https://pixabay.com/api/?key=33059818-517afd886c2c80b9c8f0a6272&q=${ref.current.value}&image_type=photo&pretty=true`;
+
+    //APIをたたく（データフェッチング）
+    fetch(endPonintURL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data.hits);
+        setFetchData(data.hits);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>My Pixabay</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="画像を検索する" ref={ref} />
+      </form>
+      <ImageGallery fetchData={fetchData} />
     </div>
   );
 }
